dce773c0cd37864594932e527e2fd2801156bad3
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-06-29 21:29:16 -0400

    mocks

--- script2.js
+++ script2.js
@@ -4,7 +4,7 @@ const getPeoplePromise = (fetch) => {
   return fetch('https://swapi.dev/api/people/')
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       return {
         count: data.count,
         results: data.results,
